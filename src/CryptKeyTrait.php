73ca7478a1b97ed6c1175aed17b7502f9a81f88d
Andi Rückauer <arueckauer@gmail.com>
2021-10-01 17:29:10 +0200

    Remove file headers
    Signed-off-by: Andi Rückauer <arueckauer@gmail.com>

--- src/CryptKeyTrait.php
+++ src/CryptKeyTrait.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/mezzio/mezzio-authentication-oauth2 for the canonical source repository
- * @copyright https://github.com/mezzio/mezzio-authentication-oauth2/blob/master/COPYRIGHT.md
- * @license   https://github.com/mezzio/mezzio-authentication-oauth2/blob/master/LICENSE.md New BSD License
- */
-
 namespace Mezzio\Authentication\OAuth2;
 
 use League\OAuth2\Server\CryptKey;
--- src/Repository/Pdo/RefreshTokenRepository.php
+++ src/Repository/Pdo/RefreshTokenRepository.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/mezzio/mezzio-authentication-oauth2 for the canonical source repository
- * @copyright https://github.com/mezzio/mezzio-authentication-oauth2/blob/master/COPYRIGHT.md
- * @license   https://github.com/mezzio/mezzio-authentication-oauth2/blob/master/LICENSE.md New BSD License
- */
-
 declare(strict_types=1);
 
 namespace Mezzio\Authentication\OAuth2\Repository\Pdo;
