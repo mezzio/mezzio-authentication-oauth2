afc3b7e66d2ea36aebc9c6da44f436c4d9fcd8a8
George Steel <george@net-glue.co.uk>
2022-10-14 20:33:00 +0100

    Improve tests using a stub instead of a mock
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- test/Entity/RevokableTraitTest.php
+++ test/Entity/RevokableTraitTest.php
@@ -4,27 +4,24 @@ declare(strict_types=1);
 
 namespace MezzioTest\Authentication\OAuth2\Entity;
 
-use Mezzio\Authentication\OAuth2\Entity\RevokableTrait;
-use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class RevokableTraitTest extends TestCase
 {
-    /** @var MockObject */
-    private object $trait;
+    private RevokableTraitStub $trait;
 
     protected function setUp(): void
     {
-        $this->trait = $this->getMockForTrait(RevokableTrait::class);
+        $this->trait = new RevokableTraitStub();
     }
 
-    public function testSetRevokedToTrue()
+    public function testSetRevokedToTrue(): void
     {
         $this->trait->setRevoked(true);
         $this->assertTrue($this->trait->isRevoked());
     }
 
-    public function testSetRevokedToFalse()
+    public function testSetRevokedToFalse(): void
     {
         $this->trait->setRevoked(false);
         $this->assertFalse($this->trait->isRevoked());
---
+++ test/Entity/TimestampableTraitStub.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MezzioTest\Authentication\OAuth2\Entity;
+
+use Mezzio\Authentication\OAuth2\Entity\TimestampableTrait;
+
+final class TimestampableTraitStub
+{
+    use TimestampableTrait;
+}
